3385486b7e83e565df224278969c7d928c6c173c
Erik21-Unam <jerik67@gmail.com>
2020-04-08 18:43:03 -0500

    Se creo estructura html inicial

--- WebSite/index.php
+++ WebSite/index.php
@@ -9,9 +9,45 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!-- Compiled and minified CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="css/styles.css">
     </head>
 
     <body>
+        <header>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Inicio</a></li>
+                        <li><a href="badges.html">Documentos</a></li>
+                        <li><a href="collapsible.html">Contacto</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <div class="container">
+            <div class="row">
+                <div class="col s8">8</div>
+                <div class="col s4">4</div>
+            </div>    
+        </div>
+
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Footer Content</h5>
+                        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-copyright">
+                <div class="container"><small>© 2020 Copyright Text</small></div>
+            </div>
+        </footer>
+
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
