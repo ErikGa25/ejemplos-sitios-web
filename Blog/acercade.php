cd56f18bf58ba58854ebbe71e3024cd9a09a4ff0
Erik21-Unam <jerik67@gmail.com>
2020-04-27 14:11:50 -0500

    Cambios en el menu del sitio

--- Blog/acercade.php
+++ Blog/acercade.php
@@ -14,9 +14,48 @@
 <body>
 	<div class="container-fluid">
 		<div class="container-fluid">
-			<?php
-				include 'menu-principal.php';
-			?>
+			<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Menú</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							</button>
+						<a class="navbar-brand" href="index.php">Tutoriales TI</a>
+					</div>
+
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<form class="navbar-form navbar-left">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Buscar en este sitio">
+							</div>
+							<button type="submit" class="btn btn-default">Buscar</button>
+						</form>
+
+						<ul class="nav navbar-nav navbar-right">
+							<li ><a href="index.php">Inicio<span class="sr-only">Inicio</span></a></li>
+							<li><a href="acercade.php">Acerca de mi</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categorias <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Base de datos</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Programación</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Desarrollo Web</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Sistemas Operativos</a></li>
+								</ul>
+							</li>
+							<li><a href="portafolio.php">Portafolio</a></li>
+							<li><a href="contacto.php">Contacto</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
 			<div class="container">
 				<img src="img/perfil.jpg" class="img-thumbnail" />
 
